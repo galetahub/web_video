c99dd50ea57ad96ad90793bcc2fe5352d9327ac3
superp <superp1987@gmail.com>
2011-02-21 18:30:02 +0200

    add resolution to stream

--- lib/web_video/stream.rb
+++ lib/web_video/stream.rb
@@ -1,6 +1,6 @@
 module WebVideo
   class Stream
-    attr_accessor :type, :codec, :details
+    attr_accessor :type, :codec, :details, :resolution
     
     #
     # stream = Stream.new :type => 'Video', :codec => 'mpeg4', :details => 'yuv420p, 640x480 [PAR 1:1 DAR 4:3], 30 tbr, 30 tbn, 30 tbc'
@@ -13,6 +13,7 @@ module WebVideo
       @type = options[:type].to_s.downcase
       @codec = options[:codec]
       @details = options[:details]
+      @resolution = extract_resolution_from_details(@details)
     end
     
     def video?
@@ -22,5 +23,20 @@ module WebVideo
     def audio?
       @type == 'audio'
     end
+    
+    def width
+      @resolution ? @resolution[/^\d+/] : nil
+    end
+    
+    def height
+      @resolution ? @resolution[/\d+$/] : nil
+    end
+    
+    private
+      def extract_resolution_from_details(details)
+        return nil if details.blank?
+        
+        details[/\s\d+x\d+\s/].strip 
+      end
   end
 end
